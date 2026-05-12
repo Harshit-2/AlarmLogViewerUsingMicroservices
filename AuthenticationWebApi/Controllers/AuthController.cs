74bbefb8090bc7204bd7272dcdbe88b23cbe9559
Harshit <harshit242003@hotmail.com>
2026-05-12 12:49:33 +0530

    spacing fix

--- AuthenticationWebApi/Controllers/AuthController.cs
+++ AuthenticationWebApi/Controllers/AuthController.cs
@@ -20,8 +20,8 @@ namespace AuthenticationWebApi.Controllers
                 new Claim(ClaimTypes.Role, role)
             };
             var token = new JwtSecurityToken(
-                issuer: "https://www.snrao.com",
-                audience: "https://www.snrao.com",
+                issuer: "https://www.project.com",
+                audience: "https://www.project.com",
                 expires: DateTime.Now.AddHours(2),
                 signingCredentials: credentials,
                 claims: claims
--- RoomViewerAPI/Program.cs
+++ RoomViewerAPI/Program.cs
@@ -41,7 +41,10 @@ builder.Services.AddSwaggerGen(options =>
     };
 });
 
-var app = builder.Build(); app.UseSwagger(); app.UseSwaggerUI();// Configure the HTTP request pipeline.
+var app = builder.Build(); 
+app.UseSwagger(); 
+app.UseSwaggerUI();// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
