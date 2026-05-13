2b40c206aa929c267e38f34bf2b78d56e7f83e95
Harshit <harshit242003@hotmail.com>
2026-05-13 11:07:39 +0530

    snrao to google

--- UserViewerAPI/Program.cs
+++ UserViewerAPI/Program.cs
@@ -35,8 +35,8 @@ builder.Services.AddSwaggerGen(options =>
         ValidateIssuer = true,
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
-        ValidIssuer = "https://www.snrao.com",
-        ValidAudience = "https://www.snrao.com",
+        ValidIssuer = "https://www.google.com",
+        ValidAudience = "https://www.google.com",
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("I am Bond, James Bond. I am the best spy in the world. I am invincible."))
     };
 });
